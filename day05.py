01ae4a758c190b732eadc19fd4aff602a2d190be
Hobbes <117660887+drhobbes@users.noreply.github.com>
2024-12-05 18:54:36 -0600

    Update day05.py

--- day05.py
+++ day05.py
@@ -1,44 +1,44 @@
 dependencies = {}
 updates = []
 with open('day05_input.txt','r') as f:
-	for line in f:
-		if '|' in line:
-			first_page = int(line.split('|')[0])
-			if first_page in dependencies:
-				dependencies[first_page].append(int(line.strip().split('|')[1]))
-			else:
-				dependencies[first_page] = [int(line.strip().split('|')[1])]
-		elif len(line.strip()) != 0:
-			updates.append([int(x) for x in line.strip().split(',')])
+  for line in f:
+    if '|' in line:
+      first_page = int(line.split('|')[0])
+      if first_page in dependencies:
+        dependencies[first_page].append(int(line.strip().split('|')[1]))
+      else:
+        dependencies[first_page] = [int(line.strip().split('|')[1])]
+    elif len(line.strip()) != 0:
+      updates.append([int(x) for x in line.strip().split(',')])
 
 def correct_order(update):
-	for i in range(1,len(update)):
-		page = update[i]
-		if page in dependencies:
-			for other_page in dependencies[page]:
-				if other_page in update[:i]: return False
-	return True
+  for i in range(1,len(update)):
+    page = update[i]
+    if page in dependencies:
+      for other_page in dependencies[page]:
+        if other_page in update[:i]: return False
+  return True
 
 def fix_order(update):
-	i = 1
-	while i < len(update):
-		page = update[i]
-		if page in dependencies:
-			for other_page in dependencies[page]:
-				if other_page in update[:i]:
-					j = update.index(other_page)
-					update[j] = page
-					update[i] = other_page
-					i = 0
-		i += 1
+  i = 1
+  while i < len(update):
+    page = update[i]
+    if page in dependencies:
+      for other_page in dependencies[page]:
+        if other_page in update[:i]:
+          j = update.index(other_page)
+          update[j] = page
+          update[i] = other_page
+          i = 0
+    i += 1
 
 total = 0
 p2_total = 0
 for update in updates:
-	if correct_order(update):
-		total += update[len(update)//2]
-	else:
-		fix_order(update)
-		p2_total += update[len(update)//2]
+  if correct_order(update):
+    total += update[len(update)//2]
+  else:
+    fix_order(update)
+    p2_total += update[len(update)//2]
 print('part 1:',total)
 print('part 2:',p2_total)
