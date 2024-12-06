44b7bfdc7e12ecab9f1cbe82078b60b752e9c5f4
Hobbes <legault@wisc.edu>
2024-12-06 07:42:00 -0600

    day06 part1

--- day06.py
+++ day06.py
@@ -1,56 +1,59 @@
 layout = []
 guard_start = []
-visited = [] # TODO: stop double-counting things
+visited = []
 
 with open('day06_input.txt','r') as f:
-	line_index = 0
-	for line in f:
-		if '^' in line:
-			guard_start = [line_index, line.find('^')]
-		layout.append(line.strip())
-		line_index += 1
+  line_index = 0
+  for line in f:
+    if '^' in line:
+      guard_start = [line_index, line.find('^')]
+    layout.append(line.strip())
+    visited.append([' ']*len(line.strip()))
+    line_index += 1
+    
+visited[guard_start[0]][guard_start[1]] = 'X'
 
 def move_up(guard):
-	i = guard[0]-1
-	count = 0
-	while i >= 0 and layout[i][guard[1]] != '#':
-		count += 1
-		i -= 1
-	guard[0] = -1 if i == -1 else i+1
-	return count if i != -1 else count-1
+  i = guard[0]-1
+  while i >= 0 and layout[i][guard[1]] != '#':
+    visited[i][guard[1]] = 'X'
+    i -= 1
+  guard[0] = -1 if i == -1 else i+1
 
 def move_down(guard):
-	i = guard[0]+1
-	count = 0
-	while i < len(layout) and layout[i][guard[1]] != '#':
-		count += 1
-		i += 1
-	guard[0] = -1 if i == len(layout) else i-1
-	return count if i != len(layout) else count-1
+  i = guard[0]+1
+  while i < len(layout) and layout[i][guard[1]] != '#':
+    visited[i][guard[1]] = 'X'
+    i += 1
+  guard[0] = -1 if i == len(layout) else i-1
 
 def move_right(guard):
-	path = layout[guard[0]][guard[1]:]
-	stop = path.find('#')
-	guard[1] = -1 if stop == -1 else guard[1]+stop-1
-	return stop-1 if stop != -1 else len(path-1)
+  j = guard[1]+1
+  while j < len(layout[guard[0]]) and layout[guard[0]][j] != '#':
+    visited[guard[0]][j] = 'X'
+    j += 1
+  guard[1] = -1 if j == len(layout) else j-1
 
 def move_left(guard):
-	path = layout[guard[0]][:guard[1]]
-	stop = path[::-1].find('#')
-	guard[1] = -1 if stop == -1 else guard[1]-stop
-	return stop-1 if stop != -1 else len(path)
+  j = guard[1]-1
+  while j >= 0 and layout[guard[0]][j] != '#':
+    visited[guard[0]][j] = 'X'
+    j -= 1
+  guard[1] = -1 if j == -1 else j+1
 
-total = 0
 moves = 0
 while guard_start[0] != -1 and guard_start[1] != -1:
-	if moves % 4 == 0:
-		total += move_up(guard_start)
-	elif moves % 4 == 1:
-		total += move_right(guard_start)
-	elif moves % 4 == 2:
-		total += move_down(guard_start)
-	else:
-		total += move_left(guard_start)
-	moves += 1
+  if moves % 4 == 0:
+    move_up(guard_start)
+  elif moves % 4 == 1:
+    move_right(guard_start)
+  elif moves % 4 == 2:
+    move_down(guard_start)
+  else:
+    move_left(guard_start)
+  moves += 1
 
+total = 0
+for row in visited:
+  total += row.count('X')
 print('part 1:',total)
