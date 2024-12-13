e463ede6ba4fe599c28da59944a6e5ebeb3197fd
Hobbes <117660887+drhobbes@users.noreply.github.com>
2024-12-13 13:05:18 -0600

    day13 solution

--- day13.py
+++ day13.py
@@ -14,7 +14,7 @@ with open('day13_input.txt','r') as f:
       elif line.startswith('Prize'):
         x = int(parts[0].strip().split('=')[1])
         y = int(parts[1].strip().split('=')[1])
-        machine['Prize'] = (x,y)
+        machine['Prize'] = [x,y]
         machines.append(machine)
 
 def push_A(machine, claw_loc):
@@ -41,3 +41,20 @@ for machine in machines:
   if claw_loc == [0,0]:
     total += machine_cost
 print('part 1:',total)
+
+# alright we can't do this one brute force, time to solve some systems of equations
+import numpy as np
+
+total = 0
+which_machine = 1
+for machine in machines:
+  machine['Prize'][0] += 10000000000000
+  machine['Prize'][1] += 10000000000000
+  a = np.array([[machine['A'][0],machine['B'][0]],[machine['A'][1],machine['B'][1]]])
+  b = np.array(machine['Prize'])
+  x = np.linalg.solve(a,b)
+  # deal with floating point errors:
+  if abs(x[0] - np.round(x[0])) < 0.001 and abs(x[1] - np.round(x[1])) < 0.001:
+    total += 3*x[0] + x[1]
+  which_machine += 1
+print('part 2:',total)
